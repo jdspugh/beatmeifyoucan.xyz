fea160706b254d7599c97cd810b087ac15d47fbf
Jonathan Pugh <jdspugh@users.noreply.github.com>
2022-08-28 13:08:59 +0800

    gas optimisation

--- smart-contract/combat.sol
+++ smart-contract/combat.sol
@@ -6,39 +6,45 @@ contract Combat{
       uint256 b;
       address a1;
       //uint256 a2;
-      int16 m1;
-      int16 m2;
+      uint16 m1; uint16 m2;
     }
-    uint32 public count=0;
-    mapping(uint32=>Game) public H;
+    uint32 private count=0;
+    mapping(uint32=>Game) private H;
+    address private deployer;
 
-    event Open(int16 moves, uint256 bet) anonymous;
-    event Close(uint32 n, int16 moves) anonymous;
+    event Open(uint16 moves, uint256 bet) anonymous;
+    event Close(uint32 n, uint16 moves) anonymous;
 
     error ChooseOpenGame();// 1
     error SendBetAmount();// 2
 
-    function open(int16 moves) external payable {
-        H[count++]=Game({b:msg.value, a1:msg.sender, m1:moves, m2:-1});
+    function open(uint16 moves) external payable {
+        H[count++]=Game({b:msg.value, a1:msg.sender, m1:moves, m2:65535});
         emit Open(moves, msg.value);
     }
-    function close(uint32 n,int16 moves) external payable {
+    function close(uint32 n,uint16 moves) external payable {
         Game memory g=H[n];
-        if(-1 != g.m2) revert ChooseOpenGame();
+        if(65535 != g.m2) revert ChooseOpenGame();
         if(g.b != msg.value) revert SendBetAmount();
 
         H[n].m2=moves;
-        if(g.m1 == moves) {
-            payable(g.a1).transfer(g.b);
-            payable(msg.sender).transfer(g.b);
-        } else {
-            payable((g.m1>moves)?g.a1:msg.sender).transfer(g.b*2);
+        uint256 a=uint256(g.m1); uint256 b=uint256(moves);
+        uint256 w=6;//w>0 p1 wins, w<0 p2 wins
+        for(uint256 i=6;i>0;i--){
+            uint256 c=a&0x3; uint256 d=b&0x3;
+            if(c!=d)if((0==c && 2==d) || (1==c && 0==d) || (2==c && 1==d)){w++;}else{w--;}//0=R,1=P,2=S
+            a>>=2; b>>=2;
         }
-        emit Close(n, moves);
+        if(6==w) {
+            payable(g.a1).transfer(g.b); payable(msg.sender).transfer(g.b);
+        } else {
+            payable(w>6?g.a1:msg.sender).transfer(g.b*2);
+        }
+        emit Close(n, uint16(moves));
     }
     
-    // not needed:
-    function balance() external view returns(uint256) {
-        return address(this).balance;
-    }
+    // // not needed:
+    // function balance() external view returns(uint256) {
+    //     return address(this).balance;
+    // }
 }
