9ee7741ca8a5598ba56a993ddc81fc1b0b4c525a
Jonathan Pugh <jdspugh@users.noreply.github.com>
2022-09-08 14:07:53 +0800

    Game table updating correctly

--- cloud/functions.js
+++ cloud/functions.js
@@ -1,21 +1,22 @@
 function L(...a){return logger.info(JSON.stringify(a))}
+const M=Moralis
 
-Moralis.Cloud.define('gamesOpen',async req=>{
-/*  const query=new Moralis.Query('Review')
-  query.equalTo('movie', request.params.movie)
-  const results=await query.find()
-  let sum=0
-  for(let i=0;i<results.length;++i){
-    sum+=results[i].get('stars')
-  }
-  return sum/results.length*/
+M.Cloud.define('gamesOpen',async()=>{
   return 'hello!'
 })
 
-Moralis.Cloud.afterSave('updates',async()=>{
-  L('afterSave(updates)')
-  web3 = new Moralis.Web3(new Moralis.Web3.providers.HttpProvider('https://evm-t3.cronos.org'))
-  const contract = new web3.eth.Contract(CONTRACT.abi,CONTRACT.address)
-  const r=await contract.methods.H(0).call().catch(e=>L('e=',e))
-  L('r=',r)
+M.Cloud.afterSave('updates',async(req)=>{
+  const n=req.object.get('n')
+  //get game
+  const w=new M.Web3(new M.Web3.providers.HttpProvider('https://evm-t3.cronos.org'))
+  const c=new w.eth.Contract(CONTRACT.abi,CONTRACT.address)
+  const r=await c.methods.H(n).call()
+  //store game
+  const Game=M.Object.extend('Game')
+  const Q=new M.Query(Game)
+  const q=Q.equalTo('n',n)
+  const gs=await q.find()
+  let g=gs[0]
+  if(!g)g=new Game()
+  await g.save({n:n,t:r.t,b:r.b,a1:r.a1,a2:r.a2,d:r.d,m1:r.m1,m2:r.m2})
 })
