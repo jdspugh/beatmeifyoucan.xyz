60a5ee1f7d60cd5c9d8b83273ca920cf1b420a0c
Jonathan Pugh <jdspugh@users.noreply.github.com>
2022-10-12 14:01:49 +0800

    removed debugging

--- server/wss.mjs
+++ server/wss.mjs
@@ -6,6 +6,6 @@ const w=new WebSocketServer({host:'127.0.0.1',port:8091})
 let n=0
 w.on('connection',s=>{
   s.n=n++
-  s.on('message',m=>{D();w.clients.forEach(c=>{D(c.n);if(s!=c)c.send(`${s.n} ${m}`)})})
+  s.on('message',m=>w.clients.forEach(c=>{if(s!=c)c.send(`${s.n} ${m}`)}))
 })
 w.on('close',e=>console.log('close e=',e))
