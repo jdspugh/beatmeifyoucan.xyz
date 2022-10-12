688606fae26632fdb6b163a39cee15b68752119e
Jonathan Pugh <jdspugh@users.noreply.github.com>
2022-10-12 12:53:32 +0800

    added player names

---
+++ server/wss.mjs
@@ -0,0 +1,11 @@
+import{WebSocketServer}from'ws'
+
+const D=console.log
+const w=new WebSocketServer({host:'127.0.0.1',port:8091})
+
+let n=0
+w.on('connection',s=>{
+  s.n=n++
+  s.on('message',m=>{D();w.clients.forEach(c=>{D(c.n);if(s!=c)c.send(`${s.n} ${m}`)})})
+})
+w.on('close',e=>console.log('close e=',e))
