9abdda1c6f6bb9f2839c27af5e64ba575035b3e7
Jonathan Pugh <jdspugh@users.noreply.github.com>
2022-10-18 19:52:27 +0800

    improved network stability and notifications

--- server/wss.mjs
+++ server/wss.mjs
@@ -8,7 +8,7 @@ let n=0,m={}
 w.on('connection',(s,q)=>{
   const u=q.url.slice(q.url.lastIndexOf('/')+1);s.n=m[u]||n,m[u]=n++;s.u=u// restore session from client key
   s.on('message',m=>{D('s.n=',s.n,'m=',m.toString());w.clients.forEach(c=>{D('c.n=',c.n);if(s!=c)c.send(`${s.n} ${m}`)});s.t=Date.now()})// broadcast
-  setTimeout(function f(){Date.now()-s.t<3e4?setTimeout(f,1e4):(D('timeout session s.n=',s.n,'w.clients.size=',w.clients.size),delete m[s.u],s.terminate(),s=null)},3e4)// 30s timeout
+  setTimeout(function f(){Date.now()-s.t<3e4?setTimeout(f,1e4):(D('timeout session s.n=',s.n,'w.clients.size=',w.clients.size),delete m[s.u],s.terminate(),s=null)},36e5)// 60m timeout
 })
 //w.on('close',e=>console.log('close e=',e))// never triggered. ws bug?
 
